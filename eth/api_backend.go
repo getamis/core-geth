f6826d02bca9f34a6f6e46bd32f0d3238464c764
mark.lin <mark@maicoin.com>
2019-08-20 14:25:02 +0800

    *: add GetTransferLogs/GetTotalDifficulty/GetBlockReceipts/SubscribeNewQueuedTxsEvent apis

--- eth/api_backend.go
+++ eth/api_backend.go
@@ -33,6 +33,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/txpool"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/eth/downloader"
 	"github.com/ethereum/go-ethereum/eth/gasprice"
 	"github.com/ethereum/go-ethereum/eth/tracers"
 	"github.com/ethereum/go-ethereum/ethdb"
@@ -356,6 +357,10 @@ func (b *EthAPIBackend) SyncProgress() ethereum.SyncProgress {
 		prog.TxIndexRemainingBlocks = txProg.Remaining
 	}
 	return prog
+}
+
+func (b *EthAPIBackend) Downloader() *downloader.Downloader {
+	return b.eth.Downloader()
 }
 
 func (b *EthAPIBackend) SuggestGasTipCap(ctx context.Context) (*big.Int, error) {
--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -351,6 +351,14 @@ func (ec *Client) SubscribeNewSideHead(ctx context.Context, ch chan<- *types.Hea
 	return ec.c.EthSubscribe(ctx, ch, "newSideHeads")
 }
 
+func (ec *Client) SubscribePendingTransactions(ctx context.Context, ch chan<- common.Hash) (ethereum.Subscription, error) {
+	return ec.c.EthSubscribe(ctx, ch, "newPendingTransactions")
+}
+
+func (ec *Client) SubscribeQueuedTransactions(ctx context.Context, ch chan<- *types.Transaction) (ethereum.Subscription, error) {
+	return ec.c.EthSubscribe(ctx, ch, "newQueuedTransactions")
+}
+
 // State Access
 
 // NetworkID returns the network ID for this client.
