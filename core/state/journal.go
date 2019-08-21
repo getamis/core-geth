c06b1fc793b637ebdfa8f7144618c8c344e14ea2
mark.lin <mark@maicoin.com>
2019-08-21 09:28:12 +0800

    core/state: keep transfer log in state db

--- core/state/journal.go
+++ core/state/journal.go
@@ -138,6 +138,9 @@ type (
 		address *common.Address
 		slot    *common.Hash
 	}
+	addTransferLogChange struct {
+		txhash common.Hash
+	}
 )
 
 func (ch createObjectChange) revert(s *StateDB) {
@@ -265,5 +268,18 @@ func (ch accessListAddSlotChange) revert(s *StateDB) {
 }
 
 func (ch accessListAddSlotChange) dirtied() *common.Address {
+	return nil
+}
+
+func (ch addTransferLogChange) revert(s *StateDB) {
+	transferLogs := s.transferLogs[ch.txhash]
+	if len(transferLogs) == 1 {
+		delete(s.transferLogs, ch.txhash)
+	} else {
+		s.transferLogs[ch.txhash] = transferLogs[:len(transferLogs)-1]
+	}
+}
+
+func (ch addTransferLogChange) dirtied() *common.Address {
 	return nil
 }
