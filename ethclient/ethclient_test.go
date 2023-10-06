600b202a2fd003832f0939e2c3430d6e7f43ee17
Cara Wang <cara@maicoin.com>
2023-10-06 17:13:26 +0800

    ethclient: fix unit test

--- ethclient/ethclient_test.go
+++ ethclient/ethclient_test.go
@@ -1321,4 +1321,11 @@ var allRPCMethods = []string{
 	"txpool_status",
 	"web3_clientVersion",
 	"web3_sha3",
+	"debug_getBlockReceipts",
+	"debug_getTransferLogs",
+	"debug_getTrieFlushInterval",
+	"debug_setTrieFlushInterval",
+	"eth_getBalanceByHash",
+	"eth_newQueuedTransactionFilter",
+	"eth_newQueuedTransactions",
 }
