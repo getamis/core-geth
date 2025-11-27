getamis/go-ethereum

*: add GetTransferLogs/GetBlockReceipts apis

internal/ethapi/api.go
@@ -334,6 +334,15 @@ func (api *BlockChainAPI) GetBalance(ctx context.Context, address common.Address
 	return (*hexutil.Big)(b), state.Error()
 }
 
+// GetBalanceByHash returns the amount of wei for the given address in the state of the
+// given block hash. The rpc.LatestBlockNumber and rpc.PendingBlockNumber meta
+// block numbers are also allowed.
+// Deprecated: can be replaced by GetBalance
+func (api *BlockChainAPI) GetBalanceByHash(ctx context.Context, address common.Address, blockHash common.Hash) (*hexutil.Big, error) {
+	hash := rpc.BlockNumberOrHashWithHash(blockHash, false)
+	return api.GetBalance(ctx, address, hash)
+}
+
 // AccountResult structs for GetProof
 type AccountResult struct {
 	Address      common.Address  `json:"address"`

getamis/core-geth

internal/ethapi/api.go
@@ -658,6 +658,15 @@ func (s *BlockChainAPI) GetBalance(ctx context.Context, address common.Address, 
 	return (*hexutil.Big)(b), state.Error()
 }
 
+// GetBalanceByHash returns the amount of wei for the given address in the state of the
+// given block hash. The rpc.LatestBlockNumber and rpc.PendingBlockNumber meta
+// block numbers are also allowed.
+// Deprecated: can be replaced by GetBalance
+func (s *BlockChainAPI) GetBalanceByHash(ctx context.Context, address common.Address, blockHash common.Hash) (*hexutil.Big, error) {
+	hash := rpc.BlockNumberOrHashWithHash(blockHash, false)
+	return s.GetBalance(ctx, address, hash)
+}
+
 // AccountResult structs for GetProof
 type AccountResult struct {
 	Address      common.Address  `json:"address"`